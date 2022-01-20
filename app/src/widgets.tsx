acc26faa88baa77cda29d955d1b2839d81313812
Matthew Molloy <whamtet@gmail.com>
2022-01-20 20:42:56 +0900

    first three widgets working

---
+++ app/src/widgets.tsx
@@ -0,0 +1,17 @@
+import './widgets.css';
+import notallowed from './notallowed.svg';
+
+export const playButton = (
+    <button className="btn btn-available">
+        <i className="fa fa-play" />
+        Play Trailer</button>
+);
+
+// we use inline css because we wish to bundle image
+const cursor = `url("${notallowed}"), not-allowed`;
+export const unavailableButton = (
+    <button className="btn btn-unavailable" style={{cursor}}>
+            <i className="fa fa-play" /> Trailer Unavailable
+    </button>
+);
+
