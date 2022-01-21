66a4faa5ac6e7842973cd37e8ce203749ae34604
Matthew Molloy <whamtet@gmail.com>
2022-01-21 20:11:24 +0900

    component fully reactive

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
 import './App.css';
-import * as Search from './search';
+import {SearchBar} from './search';
 
 function App() {
   return (
     <div className="App">
-      {Search.searchBar}
+      <SearchBar />
     </div>
   );
 }
--- app/src/search.tsx
+++ app/src/search.tsx
@@ -1,12 +1,48 @@
-import './search.css';
 import search from './assets/Icons-search.svg';
+import './search.css';
 
-export const searchBar = (
-    <div className="search-bar">
-        <img src={search} />
-        <input placeholder="Search for movies, TV shows or people..." />
-        <button>
-            Search
-        </button>
-    </div>
-);
+import * as React from 'react';
+
+interface State {
+    query: string;
+}
+
+export class SearchBar extends React.Component<object, State> {
+    state = {
+        query: ''
+    }
+    updateQuery(query: string) {
+        this.setState({query});
+    }
+    searchSection() {
+        const {query} = this.state;
+        return (
+            <div className="search-input">
+                <img src={search}/>
+                <input
+                    onChange={e => this.updateQuery(e.target.value)}
+                    placeholder="Search for movies, TV shows or people..."
+                    value={query}
+                />
+                {query ? (
+                    <div
+                        className="clear-text"
+                        onClick={() => this.updateQuery('')}
+                    >
+                        Clear
+                    </div>
+                ) : null}
+            </div>
+        );
+    }
+    render() {
+        return (
+            <div className="search-bar">
+                {this.searchSection()}
+                <button>
+                    Search
+                </button>
+            </div>
+        );
+    }
+}
