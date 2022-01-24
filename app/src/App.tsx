c4554b6b196de92481a3c2719e25bdd0a8d2897c
Matthew Molloy <whamtet@gmail.com>
2022-01-24 13:48:36 +0900

    single result notice

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -6,17 +6,19 @@ import { filterButton, FilterOption } from './widgets/widgets';
 
 interface State {
     filter: FilterOption;
+    results: object[];
 }
 
 class App extends React.Component<object, object> {
     state = {
-        filter: FilterOption.All
+        filter: FilterOption.All,
+        results: []
     };
     setFilter(filter: FilterOption) {
         this.setState({filter});
     }
     render() {
-        const {filter} = this.state;
+        const {filter, results} = this.state;
         return (
             <div className="App">
                 <div className="header">
@@ -25,11 +27,21 @@ class App extends React.Component<object, object> {
                 </div>
 
                 <div className="contents">
-                    <SearchBar/>
-                    {filterButton(FilterOption.All, filter, () => this.setFilter(FilterOption.All))}
-                    {filterButton(FilterOption.Movies, filter, () => this.setFilter(FilterOption.Movies))}
-                    {filterButton(FilterOption.TVShows, filter, () => this.setFilter(FilterOption.TVShows))}
-                    {filterButton(FilterOption.People, filter, () => this.setFilter(FilterOption.People))}
+                    <div className="search-options">
+                        <SearchBar/>
+                        {filterButton(FilterOption.All, filter, () => this.setFilter(FilterOption.All))}
+                        {filterButton(FilterOption.Movies, filter, () => this.setFilter(FilterOption.Movies))}
+                        {filterButton(FilterOption.TVShows, filter, () => this.setFilter(FilterOption.TVShows))}
+                        {filterButton(FilterOption.People, filter, () => this.setFilter(FilterOption.People))}
+                    </div>
+
+                    {results.length === 1 ? (
+                        <div className="single-result">
+                            1 result found
+                        </div>
+                        ) : null}
+
+                    <hr />
                 </div>
             </div>
         );
